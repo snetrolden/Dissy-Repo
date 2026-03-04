497add2c93396bda3eb0a69c27423b6e5eca53a5
snetrolden <smed_gamer@hotmail.com>
2026-03-04 15:01:28 +0100

    RSA and testing RSA finished, Missing AES

--- Handin_3/RSA_Encryption.go
+++ Handin_3/RSA_Encryption.go
@@ -8,6 +8,7 @@ import (
 
 // KeyGen generates an RSA key-pair (n ,e ,d) given a bit-lenght k
 func KeyGen(k int) (n, e, d *big.Int, err error) {
+
 	e = big.NewInt(3)
 	one := big.NewInt(1)
 
@@ -16,11 +17,13 @@ func KeyGen(k int) (n, e, d *big.Int, err error) {
 		pLen := k / 2
 		qLen := k - pLen
 
+		// preventing error with variables
 		var p, q *big.Int
+		var err error
 
 		//Finding gcd(3,p-1) = 1
 		for {
-			p, err := rand.Prime(rand.Reader, pLen)
+			p, err = rand.Prime(rand.Reader, pLen)
 			if err != nil {
 				fmt.Print("Prime could not be generated for p", err)
 				break
@@ -35,7 +38,7 @@ func KeyGen(k int) (n, e, d *big.Int, err error) {
 
 		//Finding  gcd(3, q-1) = 1
 		for {
-			q, err := rand.Prime(rand.Reader, qLen)
+			q, err = rand.Prime(rand.Reader, qLen)
 			if err != nil {
 				fmt.Print("Prime could not be generated for q", err)
 			}
@@ -55,7 +58,7 @@ func KeyGen(k int) (n, e, d *big.Int, err error) {
 			phi := new(big.Int).Mul(pMin, qMin)
 
 			// get d
-			d = new(big.Int).ModInverse(e, phi)
+			d := new(big.Int).ModInverse(e, phi)
 			if d != nil {
 				break
 			}
@@ -63,7 +66,7 @@ func KeyGen(k int) (n, e, d *big.Int, err error) {
 
 	}
 	// return key pair
-	return n, d, e, nil
+	return n, e, d, nil
 }
 
 func Encrypt(m, e, n *big.Int) *big.Int {
--- Handin_3/main.go
+++ Handin_3/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	fmt.Println("Testing RSA")
+	k := 2048
+	n, e, d, err := KeyGen(k)
+	if err != nil {
+		fmt.Print("error, KeyGen Error", err)
+		return
+	}
+
+	fmt.Printf("Generated %d-bit RSA key. \n", n.BitLen())
+
+	origMsg := big.NewInt(123456)
+
+	ciphertext := Encrypt(origMsg, e, n)
+	decryptMsg := Decrypt(ciphertext, d, n)
+
+	if origMsg.Cmp(decryptMsg) == 0 {
+		fmt.Print("Success! RSA decrypted Msg matches the original Msg")
+	} else {
+		fmt.Print("Failure! RSA decrypted Msg don't match the original Msg")
+	}
+
+}
