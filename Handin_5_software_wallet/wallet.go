ae052e95b5fdb6491e17441aa85b303a880a972b
snetrolden <smed_gamer@hotmail.com>
2026-03-19 11:47:01 +0100

    finsihed setup

--- Handin_5_software_wallet/wallet.go
+++ Handin_5_software_wallet/wallet.go
@@ -1,1 +1,10 @@
 package main
+
+func Generate(filename string, password string) string {
+
+	return "Fake-it"
+}
+
+func Sign(filename string, password string, msg []byte) string {
+	return "Signature"
+}
