8bc980ba1907ea73c0be381fa81c865ac0f8eca2
snetrolden <smed_gamer@hotmail.com>
2026-03-19 15:32:44 +0100

    template/instrution for the generate function

--- Handin_5_software_wallet/wallet.go
+++ Handin_5_software_wallet/wallet.go
@@ -1,8 +1,42 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type secretKey struct {
+	D []byte
+	N []byte
+}
+
 func Generate(filename string, password string) string {
 
-	return "Fake-it"
+	keySize := 2000
+	n, e, d, err := KeyGen(keySize)
+	if err != nil {
+		panic(fmt.Sprint("Error: ", err))
+	}
+
+	private := secretKey{
+		D: d.Bytes(),
+		N: n.Bytes(),
+	}
+
+	plaintext, err := json.Marshal(private)
+	if err != nil {
+		panic(fmt.Sprint("Marshal error: ", err))
+	}
+
+	//make a salt for protection against dictionary attacks, prevents effective guessing
+
+	//hash like there is no tomorrow for protect against brute fuckers
+
+	//Encrypt using AES (blockcipher)
+
+	// Save to a file (os.WriteFile) remember to use the FileName given in the method
+
+	return fmt.Sprint("Modulo N: ", n.String(), "\n Public key E: ", e.String())
 }
 
 func Sign(filename string, password string, msg []byte) string {
